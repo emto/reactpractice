ff4a23560710e23b690843142985486b12c8867f
Emily To <emilytoe@gmail.com>
2015-10-08 00:46:39 -0700

    fix server

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser');
 var path = require('path');
 var fs = require('fs');
 
-app.set('port', 3000);
+app.set('port', (process.env.PORT || 3000));
 app.use('/', express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
